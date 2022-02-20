9401ee9042a4a2be2ec159ede3c0b517740804a6
huskyhehe <zhou.he1@northeastern.edu>
2022-02-19 16:28:49 -0800

    add quiz1

--- Quiz_1_Person/src/Main.java
+++ Quiz_1_Person/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Student mark = new Student("Mark", "neu001", 24);
         Professor ashish = new Professor("Ashish","neu111",30);
-
+        Student mark = new Student("Mark", "neu001", 24);
+        
         ashish.giveQuiz("What's the relationship between Person and Student?");
         mark.takeQuiz("Student is the subclass of Person");
     }
