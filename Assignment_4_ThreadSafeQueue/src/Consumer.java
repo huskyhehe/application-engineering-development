5e598e370e3f885a79618487b7adc900d9838bc1
huskyhehe <zhou.he1@northeastern.edu>
2022-03-31 21:08:09 -0700

    add assignment 4

--- Assignment_4_ThreadSafeQueue/src/Consumer.java
+++ Assignment_4_ThreadSafeQueue/src/Consumer.java
@@ -9,7 +9,7 @@ public class Consumer implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
