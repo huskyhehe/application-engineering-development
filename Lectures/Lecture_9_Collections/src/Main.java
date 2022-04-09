d416bc4dc4c03e9d8bfb4f04946b10bc699c40a4
huskyhehe <zhou.he1@northeastern.edu>
2022-04-09 15:16:21 -0700

    add lecture 9

--- Lectures/Lecture_9_Collections/src/Main.java
+++ Lectures/Lecture_9_Collections/src/Main.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+        arrayListExample();
+
+    }
+
+    public static void arrayListExample() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2,-5);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(10);
+
+        list.addAll(list2);
+    }
 }
